843d26b08e0a5fb2c4a2ad0c16d7fddeffd5fe37
MosesStewart <111905359+MosesStewart@users.noreply.github.com>
2023-11-15 15:42:15 -0500

    PR for #9: GitHub actions (#12)
    * WIP for #9
    
    * starts fixing broken tests for #9
    
    * fixes tests for #9
    
    * removes stray main for #9
    
    * fixes error for #9
    
    * Update .github/workflows/pytest.yml
    
    Co-authored-by: jmshapir <jesse.m.shapiro@gmail.com>
    
    * Update .github/workflows/pytest.yml
    
    Co-authored-by: jmshapir <jesse.m.shapiro@gmail.com>
    
    * Update .github/workflows/pytest.yml
    
    Co-authored-by: jmshapir <jesse.m.shapiro@gmail.com>
    
    * edits test for #9
    
    * changed versions for #9
    
    * Update tests/test_BootstrapReport.py
    
    Co-authored-by: jmshapir <jesse.m.shapiro@gmail.com>
    
    * removes stray import for #9
    
    * removes digits for #9
    
    ---------
    
    Co-authored-by: jmshapir <jesse.m.shapiro@gmail.com>

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -3,8 +3,8 @@
 import numpy as np
 import pytest
 from scipy.stats import norm
-import BootstrapReport.checkers
-
+import warnings
+import BootstrapReport.checkers as checkers
 
 def test_check_parameters():
     with pytest.raises(Exception):
@@ -21,38 +21,31 @@ def test_check_integration_bounds():
     def normal_pdf(x):
         return norm.pdf(x, 0, 1)
 
-    with pytest.warns(None) as record:
+    with warnings.catch_warnings():
         checkers.check_integration_bounds(3, 1, 1, np.inf, normal_pdf)
-    assert len(record) == 0
 
-    with pytest.warns(None) as record:
+    with pytest.warns(UserWarning):
         checkers.check_integration_bounds(0, 1, 1, 5, normal_pdf)
+    with pytest.warns(UserWarning):
         checkers.check_integration_bounds(0.2, 1, 1, 1, normal_pdf)
+    with pytest.warns(UserWarning):
         checkers.check_integration_bounds(0, 1, 1, np.inf, normal_pdf)
-    assert len(record) == 3
-    assert str(record[0].message) == "Warning: Consider increasing C \n or using the default infinite bounds."
-
 
 def test_check_bias_accuracy():
-    with pytest.warns(None) as record:
-        assert checkers.check_bias_accuracy(0.05, 0, 1) is None
-        assert checkers.check_bias_accuracy(0.05, 0.1, 1) is None
-        assert checkers.check_bias_accuracy(0.05, 0.1, 2) is None
-    assert len(record) == 0
+    with warnings.catch_warnings():
+        checkers.check_bias_accuracy(0.05, 0, 1)
+        checkers.check_bias_accuracy(0.05, 0.1, 1)
+        checkers.check_bias_accuracy(0.05, 0.1, 2)
 
-    with pytest.warns(None) as record:
-        assert checkers.check_bias_accuracy(0.05, 0.1, 2, tau=0.001) is None
-    assert len(record) == 1
+    with pytest.warns(UserWarning):
+        checkers.check_bias_accuracy(0.05, 0.5, 2)
 
 
 def test_check_randomization():
     assert checkers.check_randomization([1, 2, 3, 4, 5]) is None
     assert checkers.check_randomization([[1, 2, 3], [1, 2, 4]]) is None
-    message = "Warning: At least two sets of random draws are identical."
-    with pytest.warns(match=message):
+    with pytest.warns(UserWarning):
         checkers.check_randomization([1, 2, 3, 4, 5, 5])
-    with pytest.warns(match=message):
-        checkers.check_randomization([[1, 2, 3], [1, 2, 3]])
         
 
 def test_check_seed():
