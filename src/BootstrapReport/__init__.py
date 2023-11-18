f73490af2730ff42e24003a2e409dea74cd824b2
MosesStewart <mosesstewart@college.harvard.edu>
2023-11-17 19:17:45 -0500

    updates readme for #15

--- src/BootstrapReport/__init__.py
+++ src/BootstrapReport/__init__.py
@@ -10,9 +10,6 @@ __all__ = []
     -----------------
     .
     ├── __init__.py
-    ├── examples -> Data calculations stored here
-        ├── __init__.py
-        ├── examples.py -> Examples file
     ├── BootstrapReport.py -> Main functions stored here
     ├── helpers.py -> Helper functions stored here
     ├── checkers.py -> Functions that check certain inputs and outputs stored here
