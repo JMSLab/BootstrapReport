88c6c36230df1f128ff0d522c6749e95b042e092
MosesStewart <mosesstewart@college.harvard.edu>
2023-12-02 13:36:44 -0500

    draft 1 for #18

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 from shiny.types import FileInfo
-import matplotlib.pyplot as plt
 import numpy as np, pandas as pd
+from pyreadr import read_r
+from scipy.io import loadmat
 from src.BootstrapReport import ObjectOfInterest
 
-app_ui = ui.page_fluid(
+app_ui = ui.page_fixed(
     ui.panel_title("Analyze a Report"),
 
     ui.layout_sidebar(
@@ -12,20 +13,20 @@ app_ui = ui.page_fluid(
       ui.panel_sidebar(
         ui.input_numeric("est", "Estimate", 0),
         ui.input_numeric("se", "Standard error", 1),
-        ui.input_file("repcsv", "Choose CSV File", accept=[".csv"], multiple=False),
-        ui.input_checkbox_group("checkboxes", "Summary Stats",
-        choices=["Row Count", "Column Count", "Column Names"],
-        selected=["Row Count", "Column Count", "Column Names"],),
+        ui.input_file("repcsv", "Choose data file", accept=[".csv", ".mat", ".dta", ".rds", ".xls", ".xlsx"], multiple=False),
         ui.output_table("summary"),
+        ui.input_text("repname", "Name of column with replicates"),
+        width = 5.2,
       ),
 
       ui.panel_main(
-        ui.output_plot("pp_plot"),
         ui.output_table("stats"),
+        ui.output_plot("pp_plot"),
+        width = 6.8,
       ),
+      height = 900
     ),
 )
-
 
 def server(input, output, session):
     @reactive.Calc
@@ -33,14 +34,22 @@ def server(input, output, session):
         file: list[FileInfo] | None = input.repcsv()
         if file is None:
             return pd.DataFrame()
-        return pd.read_csv(  # pyright: ignore[reportUnknownMemberType]
-            file[0]["datapath"]
-        )
+        elif file[0]["datapath"][-3:] == 'csv':
+            return pd.read_csv(file[0]["datapath"])
+        elif file[0]["datapath"][-3:] == 'mat':
+            return loadmat(file[0]["datapath"])
+        elif file[0]["datapath"][-3:] == 'dta':
+            return pd.read_stata(file[0]["datapath"])
+        elif file[0]["datapath"][-3:].lower() == 'rds':
+            raw_r = read_r(file[0]["datapath"])
+            return raw_r[None]
+        elif file[0]["datapath"][-3:] == 'xls' or file[0]["datapath"][-4:] == 'xlsx':
+            return pd.read_excel(file[0]["datapath"])
     
     @reactive.Calc
     def eval_ooi() -> object:
         df_rep = parsed_file()
-        rep = df_rep['replicate_value'].values
+        rep = df_rep[input.repname()].values
         ooi = ObjectOfInterest(input.est(), input.se(), rep)
         return ooi
 
@@ -63,17 +72,16 @@ def server(input, output, session):
                 "Column Names": [column_names],
             }
         )
+        return info_df
 
-        # input.checkboxes() is a list of strings; subset the columns based on the selected
-        # checkboxes
-        return info_df.loc[:, input.checkboxes()]
-
-    @render.plot(alt="pp plot")
+    @output
+    @render.plot(alt="pp plot", width = 670, height = 670)
     def pp_plot() -> object:
         ooi = eval_ooi()
-        fig = ooi.pp_plot()
+        fig = ooi.pp_plot(fontsize = 13.5, legend_fontsize = 14.5, labelsize = 18)
         return fig
     
+    @output
     @render.table
     def stats() -> object:
         ooi = eval_ooi()
